d943a6bd57e2ff38b09a5d20bf8a31e27335cc73
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-09 20:18:27 +0530

    test

--- main.tf
+++ main.tf
@@ -25,5 +25,16 @@ resource "aws_lb_target_group_attachment" "test" {
   availability_zone = "all"
 }
 
+resource "aws_lb_listener" "front_end_alb" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+
 
 
